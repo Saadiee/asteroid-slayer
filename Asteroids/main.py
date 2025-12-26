232a31cbf42a37fc819224a2b10b442750906302
Saad Ur Rehman <saad_eew@hotmail.com>
2025-12-27 01:20:58 +0500

    feat: implement CircleShape and Player classes with movement and rotation functionality

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -12,17 +13,28 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
+    updatable = pygame.sprite.Group()                                                               # hold all the objects that can be updated
+    drawable = pygame.sprite.Group()                                                                # hold all the objects that can be drawn
+    Player.containers = (updatable, drawable)                                                       # Add the Player class to the updatable and drawable groups
+    player = Player(x, y)                                                                           # instanciate a player object
+
+    while True:                                                                                     # starts game loop
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
         delta_time = clock.tick(60)
         dt = delta_time / 1000
+        screen.fill("black")
+        updatable.update(dt)                                                                        # update all the objects
+        for obj in drawable:
+            obj.draw(screen)                                                                        # draw the player on screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
